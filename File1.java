ace1dc93b420f274c0f263817d0598ffa32780b6
MohammadkaifMManiyar <114292687+MohammadkaifMManiyar@users.noreply.github.com>
2022-09-24 18:10:54 +0530

    file is edited

--- File1.java
+++ File1.java
@@ -1,6 +1,10 @@
 //journey started!
 public class File1{
   public static void main(String args[]){
-      System.out.println("Yo I did it!");
-   }
+    System.out.println("Yo I did it!");
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.println(c);
+  }
 }
